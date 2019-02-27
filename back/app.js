e95ecd7b804226ed280631b861506e0253c4b451
Víctor Bañuls <vicbansax@gmail.com>
2019-02-27 19:08:49 +0100

    mongoose

--- back/app.js
+++ back/app.js
@@ -16,7 +16,7 @@ const flash      = require("connect-flash");
     
 
 mongoose
-  .connect('mongodb://localhost/back', {useNewUrlParser: true})
+  .connect('mongodb://localhost/gameUsers', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
