2d403d56f5af3fc023a85daaeb2979c4b639637e
Víctor Bañuls <vicbansax@gmail.com>
2019-03-05 11:28:38 +0100

    Feria changes

--- front/src/Components/MiniGames/Feria.js
+++ front/src/Components/MiniGames/Feria.js
@@ -104,7 +104,7 @@ export default class Feria extends Component {
                     <p>{this.state.speedX}</p>
                     {/* <p>Score: {JSON.stringify(this.state.score)}</p> 
                     <img alt="" className="fair" src="../../../img/feriaDesktop.png">{this.state.score.map(elem => <div>{elem.id} - {elem.score}</div>)}</img>*/}
-                    {this.state.speedX === 0?<div>{this.state.score.map(elem => <div>{elem.id} - {elem.score}</div>)}</div>:
+                    {this.state.speedX === 0?<div className="desktopBackground">{this.state.score.map(elem => <div>{elem.id} - {elem.score}</div>)}</div>:
                     <div>
                         <p>SpeedX: {this.state.speedX.toFixed(2)}</p>
                         <p>SpeedY: {this.state.speedY.toFixed(2)}</p>
