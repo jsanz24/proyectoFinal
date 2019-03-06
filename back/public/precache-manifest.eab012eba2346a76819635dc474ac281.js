e8b387ba6b30cfaa5f089e4413a646c7f364895e
jsanz24 <jesus240492@gmail.com>
2019-03-06 18:49:11 +0100

    Feria y Pollas4

---
+++ back/public/precache-manifest.eab012eba2346a76819635dc474ac281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c25eb65a1c2819d1d19e51908c43080",
+    "url": "/static/media/test.7c25eb65.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d7f0c9a03098d5e76d13",
+    "url": "/static/js/main.d7f0c9a0.chunk.js"
+  },
+  {
+    "revision": "29ac5aa2cb6f7f720fee",
+    "url": "/static/js/2.29ac5aa2.chunk.js"
+  },
+  {
+    "revision": "d7f0c9a03098d5e76d13",
+    "url": "/static/css/main.8eff00bc.chunk.css"
+  },
+  {
+    "revision": "4e049059fff3e5767d8d76854131fdb4",
+    "url": "/index.html"
+  }
+];
