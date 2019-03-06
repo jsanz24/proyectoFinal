1d802879e4f03f2321edd5d5fccf8d909b132980
jsanz24 <jesus240492@gmail.com>
2019-03-06 20:25:48 +0100

    Home y Ruca2

---
+++ back/public/precache-manifest.d3dc168caf45034700584c833541f1d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90a83ba924a41260eee39ec2b48e41c3",
+    "url": "/static/media/test.90a83ba9.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "25f6c8d8c242c091f394",
+    "url": "/static/js/main.25f6c8d8.chunk.js"
+  },
+  {
+    "revision": "29ac5aa2cb6f7f720fee",
+    "url": "/static/js/2.29ac5aa2.chunk.js"
+  },
+  {
+    "revision": "25f6c8d8c242c091f394",
+    "url": "/static/css/main.bf4bdbd3.chunk.css"
+  },
+  {
+    "revision": "e61554de26bb3e29d40bf387d4bcd24a",
+    "url": "/index.html"
+  }
+];
