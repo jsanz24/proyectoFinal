09ab22fd273fbae4a5ef78ba6e6cf52999e2c07c
Víctor Bañuls <vicbansax@gmail.com>
2019-03-08 02:04:27 +0100

    home finished

--- front/src/Components/Home.js
+++ front/src/Components/Home.js
@@ -4,24 +4,16 @@ import "./Home.css"
 
 
 export default class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            speedX: 0,
-        }
-        };
+    
     render() {
         return (
-            <div>
-                {this.state.speedX === 0?<div className="desktopHome"></div>:
-                <div>
-                <img className="" src="/img/home/games.png"></img>
+            <div className="backgroundHome">
+                <img className="header" src="/img/home/gamesBig.png"></img>
                 <Link to="/feria"><img className="buttons" src="/img/home/barbieHome.png" /></Link>
                 <Link to="/basket"><img className="buttons" src="/img/home/curryHome.png" /></Link>
-               
                 <Link to="/west"><img className="buttons" src="/img/home/westDesktop2home.png" /></Link>
-                </div>}
             </div>
+            
         )
     }
 }
